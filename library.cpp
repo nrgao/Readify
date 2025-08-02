4b50da2a56070badc7bc40b692f93bf5ec44739e
Nolan Gao <nrg9150@gmail.com>
2025-08-02 14:07:31 -0400

    preliminary library class

--- library.cpp
+++ library.cpp
@@ -1,3 +1,51 @@
 //
 // Created by nrg91 on 8/1/2025.
 //
+#include "library.h"
+
+void library::insertBook(string title, string author, string genre, string date int priority)
+{
+    if (bookToAuthor.find(title) == bookToAuthor.end())
+    {
+        bookToAuthor[title] = {author};
+    }
+    else
+    {
+        bookToAuthor[title].push_back(author);
+    }
+
+    if (priorityToBook.find(priority) == priorityToBook.end())
+    {
+        priorityToBook[priority] = {make_pair(title, author)};
+    }
+    else
+    {
+        priorityToBook[priority].push_back(make_pair(title, author));
+    }
+
+    // Insert priority value into maxHeap
+    maxHeap.push_back(priority);
+    int index = maxHeap.size() - 1;
+    while (index > 0 && maxHeap[index] > maxHeap[index / 2])
+    {
+        int temp = maxHeap[index];
+        maxHeap[index] = maxHeap[index / 2];
+        maxHeap[index / 2] = temp;
+        index /= 2;
+    }
+
+    string uniqueBook = title + " " + author;
+    attributes[uniqueBook] = {author, genre, date};
+
+    if (bookGraph.find(uniqueBook) == bookGraph.end())
+    {
+        int length = bookGraph.size();
+
+    }
+
+}
+
+string library::topBook()
+{
+    vector<pair<string, string>> books = priorityToBook[maxHeap[0]];
+}
--- library.h
+++ library.h
@@ -5,4 +5,30 @@
 #ifndef LIBRARY_H
 #define LIBRARY_H
 
+#include <vector>
+#include <map>
+#include <set>
+#include <string>
+using namespace std;
+
+class library
+{
+    map<string, vector<string>> bookToAuthor;
+    map<int, vector<pair<string, string>>> priorityToBook;
+    map<string, vector<string> > bookGraph;
+    vector<int> maxHeap;
+    map<string, vector<string>> attributes;
+
+    public:
+    void insertBook(string title, string author, string genre, string date, int priority);
+
+    string viewLibrary();
+
+    string topBook();
+
+    void removeBook(string title, string author);
+
+    void examineBook(string title, string author);
+
+};
 #endif //LIBRARY_H

ad26e6399448e671d3021c306f860d54ab17387e
Nolan Gao <nrg9150@gmail.com>
2025-08-04 01:33:27 -0400

    improved readability and added comments

--- bookGraph.cpp
+++ bookGraph.cpp
@@ -6,51 +6,53 @@
 #include <iostream>
 using namespace std;
 
+// insert into partial graph using attributes
 void BookGraph::insert(string title, string author, string genre, string date, string publisher, string description)
 {
-    string uniqueBook = title + " " + author;
-    attributes[uniqueBook] = {author, genre, publisher, date, description};
+    string uniqueBook = title + " " + author; // get unique book id
+    attributes[uniqueBook] = {author, genre, publisher, date, description}; // set attributes in map
 }
 
-vector<string> BookGraph::recommend(string title, string author)
+vector<string> BookGraph::recommend(string title, string author) // recommend book based on simscore
 {
-    string id = title + " " + author;
-    if (attributes.find(id) == attributes.end())
+    string id = title + " " + author; // get unique id
+    if (attributes.find(id) == attributes.end()) // if not in graph
         return {};
-    vector<string> current = attributes[id];
+    vector<string> current = attributes[id]; // get attributes
     int simscore;
     int maxscore = -100;
     string maxid;
-    for (auto it = attributes.begin(); it != attributes.end(); it++)
+    for (auto it = attributes.begin(); it != attributes.end(); it++) // check against all other books
     {
         simscore = 0;
-        if (it -> first != id)
+        if (it -> first != id) // compute simscore using weighted sum
         {
             vector<string> details = it -> second;
-            if (details[0] == current[0])
+            if (details[0] == current[0]) // shared author
             {
                 simscore += 7;
             }
-            if (details[1] == current[1])
+            if (details[1] == current[1]) // shared genre
             {
                 simscore += 10;
             }
-            if (details[2] == current[2])
+            if (details[2] == current[2]) // shared publisher
             {
                 simscore += 1;
             }
 
-            int timediff = abs(stoi(details[3]) - stoi(current[3]));
+            int timediff = abs(stoi(details[3]) - stoi(current[3])); // publication date difference
 
             simscore -= timediff / 10.0;
         }
-        if (maxscore < simscore)
+        if (maxscore < simscore) // update if necessary
         {
             maxscore = simscore;
             maxid = it -> first;
         }
     }
 
+    // get most similar and print
     string mostsimilar = maxid;
     int space = mostsimilar.rfind(' ');
     string booktitle = mostsimilar.substr(0, space);
@@ -59,7 +61,7 @@ vector<string> BookGraph::recommend(string title, string author)
     return {booktitle, authorname, attributes[mostsimilar][1], attributes[mostsimilar][2], attributes[mostsimilar][3], attributes[mostsimilar][4]};
 }
 
-vector <string> BookGraph::getAttributes(string id)
+vector <string> BookGraph::getAttributes(string id) // get attributes of a particular book
 {
     if (attributes.find(id) != attributes.end())
         return attributes[id];
--- bookGraph.h
+++ bookGraph.h
@@ -11,13 +11,16 @@ using namespace std;
 
 class BookGraph
 {
-    map<string, vector<pair<string, double>> > data;
-    map<string, string> bookdescriptions;
-    map<string, vector<string>> attributes;
+   // map<string, vector<pair<string, double>> > data;
+    //map<string, string> bookdescriptions; // maps boo
+    map<string, vector<string>> attributes; // partial graph that maps books to attributes
 
     public:
+    // insert book into graph
     void insert(string title, string author, string genre, string date, string publisher, string description);
+    // computes edge weights and recommends the most similar book
     vector<string> recommend(string title, string author);
+    // retrieves book attributes
     vector <string> getAttributes(string id);
 
 };
